2f8df1b4032d72acdcf8c8c1edc93341fba2a70a
shachidaikon <tkroxy0815@gmail.com>
2021-01-09 03:34:24 +0900

    Home

---
+++ components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="home">
+    <v-container fluid fill-height class="home__content">
+      <v-row>
+        <transition appear>
+          <v-col class="home__content-text">
+            <h1>Ryo Takayama's</h1>
+            <h1>PortFolio</h1>
+          </v-col>
+        </transition>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+
+.home__content {
+  background: url('../assets/senro.png');
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100vh;
+
+  &-text {
+    color: white;
+    text-align: center;
+    font-size: 40px;
+    font-weight: bold;
+  }
+
+  .v-enter-active {
+    transition: opacity 1s;
+  }
+
+  .v-enter {
+    opacity: 0;
+  }
+  .v-enter-to {
+    opacity: 1;
+  }
+}
+</style>
